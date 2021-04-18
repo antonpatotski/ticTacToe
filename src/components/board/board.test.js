7e73f9807f18fdbfbe17c8287fb3b0665fa71fd9
antonpatotski <patotskianton@gmail.com>
2021-04-19 01:49:22 +0300

    Add test for board square callBack

--- src/components/board/board.test.js
+++ src/components/board/board.test.js
@@ -20,6 +20,15 @@ describe('Board', () => {
     const wrapper = shallow(<Board squares={squares}/>);
 
     expect(wrapper.find(Square).at(0).props().value).toBe('test 1');
-    expect(wrapper.find(Square).at(0).props().value).toBe('test 2');
+    expect(wrapper.find(Square).at(1).props().value).toBe('test 2');
+  });
+
+  test('should contain squares with callbacks', () => {
+    const mockFn = jest.fn(value => value);
+    const squares = [1, 2];
+    const wrapper = shallow(<Board squares={squares} onClick={mockFn}/>);
+
+    expect(wrapper.find(Square).at(0).props().onClick()).toEqual(0);
+    expect(wrapper.find(Square).at(1).props().onClick()).toEqual(1);
   });
 })
