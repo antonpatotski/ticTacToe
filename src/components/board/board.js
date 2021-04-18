516ffdc491e89e8ff86e5972551e4cb45ec6bf83
antonpatotski <patotskianton@gmail.com>
2021-04-19 01:41:13 +0300

    Add functionality for board square values

--- src/components/board/board.js
+++ src/components/board/board.js
@@ -3,7 +3,7 @@ import Square from '../square/square';
 export default ({ squares }) => {
   return (
     <>
-      {squares.map((square, i) => <Square key={i}></Square>)}
+      {squares.map((square, i) => <Square key={i} value={square}></Square>)}
     </>
   );
 }
