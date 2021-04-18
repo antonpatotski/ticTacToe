6576ec7d7f515adfa1f4e7befd0af677975e4437
antonpatotski <patotskianton@gmail.com>
2021-04-19 01:49:33 +0300

    Add functionality for board square callBack

--- src/components/board/board.js
+++ src/components/board/board.js
@@ -1,9 +1,15 @@
 import Square from '../square/square';
 
-export default ({ squares }) => {
+export default ({ squares, onClick }) => {
   return (
     <>
-      {squares.map((square, i) => <Square key={i} value={square}></Square>)}
+      {squares.map((square, i) =>
+        <Square
+          key={i}
+          value={square}
+          onClick={() => onClick(i)}>
+        </Square>
+      )}
     </>
   );
 }
