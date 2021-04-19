f296fa2bfe555fd83deed508ff8209cfc96bc37b
antonpatotski <patotskianton@gmail.com>
2021-04-19 17:52:29 +0300

    Add functionality for reset button

--- src/App.js
+++ src/App.js
@@ -8,20 +8,39 @@ import './App.scss';
 export  default () => {
   const [squares, updateSquares] = useState(Array(9).fill(null));
   const [isXNextTurn, changeIsXTurn] = useState(true);
+  const [movesHistory, updateMovesHistory] = useState([]);
   const  winner = findWinner(squares);
 
   const handleSquareClick = (i) => {
     if (winner || squares[i] !== null) return;
 
     const squaresCopy = [...squares];
+    const movesHistoryCopy = [...movesHistory];
+
     squaresCopy[i] = isXNextTurn ? 'X' : 'O';
+    movesHistoryCopy.push(i);
+    updateMovesHistory(movesHistoryCopy);
     updateSquares(squaresCopy);
     changeIsXTurn(!isXNextTurn);
   }
 
   const startNewGame = () => {
+    updateMovesHistory([]);
     updateSquares(Array(9).fill(null));
     changeIsXTurn(true);
+  }
+
+  const resetMove = () => {
+    if (movesHistory.length === 0) return;
+
+    const movesHistoryCopy = [...movesHistory];
+    const squaresCopy = [...squares];
+    const lastChangedIndex = movesHistoryCopy[movesHistoryCopy.length - 1];
+    squaresCopy[lastChangedIndex] = null;
+    movesHistoryCopy.pop();
+    updateMovesHistory(movesHistoryCopy);
+    updateSquares(squaresCopy);
+    changeIsXTurn(!isXNextTurn);
   }
 
   return (
@@ -40,6 +59,7 @@ export  default () => {
         Start new game
       </Button>
       <Button
+        onClick={resetMove}
         className="app-body__button app-body__button--reset"
         variant="info">
         Reset move
