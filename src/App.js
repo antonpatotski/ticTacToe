8b85118e78f0ee8422f277ade15c7b2dd476e23a
antonpatotski <patotskianton@gmail.com>
2021-04-19 14:29:39 +0300

    Add board in app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import Board from './components/board/board';
 import './App.scss';
 
-function App() {
+// eslint-disable-next-line
+export  default () => {
+  const arrayExample = Array(9).fill(null);
 
+  return (
+    <div className="app-body">
+      <Board squares={arrayExample}></Board>
+    </div>
+  )
 }
-
-export default App;
