2b1ca67035b70b2a0ce61654260506b9352c1d57
antonpatotski <patotskianton@gmail.com>
2021-04-19 14:29:01 +0300

    Add unit test for board define

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,16 @@
 import App from './App';
+import Board from "./components/board/board";
+
+import {shallow} from "enzyme";
 
 describe('App', () => {
   test('should be exist', () => {
     expect(App).toBeDefined();
+  });
+
+  test('should contain board', () => {
+    const wrapper = shallow(<App />);
+
+    expect(wrapper.find(Board)).toBeDefined();
   });
 })
