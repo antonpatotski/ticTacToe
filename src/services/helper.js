d882e2fa232e3fdc3e67915ae31d287b1abbe844
antonpatotski <patotskianton@gmail.com>
2021-04-19 17:04:25 +0300

    Add functionality for checking winner

--- src/services/helper.js
+++ src/services/helper.js
@@ -1,3 +1,9 @@
-export const findWinner = (squares) => {
+import { winCases} from "../constants";
 
+export const findWinner = (squares) => {
+  for (let i = 0; i < winCases.length; i++) {
+    const [a, b, c] = winCases[i];
+
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) return squares[a];
+  }
 }
